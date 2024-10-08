3c3566b707c2534fe75178cf9989ff5bff068aed
John Bernard Luna <125585580+NimoLuna@users.noreply.github.com>
2024-10-08 08:08:55 +0000

    main

--- Tasksheet114 (John Bernard N.).java
+++
@@ -1,8 +0,0 @@
-public class Tasksheet114 {
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + ((i % 2 == 0) ? " is even number" : " is odd number"));
-        }
-    }
-}
